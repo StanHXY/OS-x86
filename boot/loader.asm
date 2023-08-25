d16611914334c14aaf09c56e07d31bfdba9d4157
Ubuntu <ubuntu@ip-172-31-9-150.us-east-2.compute.internal>
2023-08-25 21:42:36 +0000

    LONG mode support

--- boot/loader.asm
+++ boot/loader.asm
@@ -2,6 +2,22 @@
 [ORG 0x7e00]
 
 start:
+    mov [DriverId],dl
+
+    ;check if input is allowed
+    mov eax,0x80000000
+    cpuid
+    cmp eax,0x80000001
+    jb NotSupport; jump if below
+
+    ; get processor features, check if LONG mode is supported
+    mov eax,0x80000001
+    cpuid
+    test edx, (1<<29)
+    jz NotSupport ; check zero flag
+    test edx,(1<<26) ;check if support 1g page
+    jz NotSupport
+
     mov ah,0x13 ;0x13(print string), ah holds function code
     mov al,1 ; al(write mode), 1 means cursor will be placed at end of string
     mov bx,0xa ; bx(character attributes), 0xa(bright green)
@@ -11,10 +27,12 @@ start:
     int 0x10 ;call BIOS interrupts, interrupt 0x10 is PRINT
 
 ; use infinite loop to halt the processor
+NotSupport:
 End:
     hlt
     jmp End
 
 
-Message:  db "Loader Starts ..."
+DriveId: db 0
+Message:  db "Long Mode Supported"
 MessageLen: equ $-Message
