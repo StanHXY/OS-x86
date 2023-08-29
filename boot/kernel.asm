62033db80840d2739b52247ddf0ed1a965295c6c
StanHXY <xhe122@syr.edu>
2023-08-29 20:17:48 +0000

    interrupt timer

--- boot/kernel.asm
+++ boot/kernel.asm
@@ -11,6 +11,15 @@ start:
     shr rax,16
     mov [rdi+8],eax
 
+    ;Timer entry
+    mov rax,Timer
+    add rdi,32*16
+    mov [rdi],ax
+    shr rax,16
+    mov [rdi+6],ax
+    shr rax,16
+    mov [rdi+8],eax
+
     lgdt [Gdt64Ptr]
     lidt [IdtPtr]
 
@@ -28,17 +37,128 @@ KernelEntry:
     xor rbx,rbx
     div rbx
 
+InitPIT:
+    mov al,(1<<2)|(3<<4)
+    out 0x43,al
+
+    mov ax,11931
+    out 0x40,al
+    mov al,ah
+    out 0x40,al
+
+;set up interrupt controller
+InitPIC:
+    mov al,0x11
+    out 0x20,al
+    out 0xa0,al
+
+    mov al,32
+    out 0x21,al
+    mov al,40
+    out 0xa1,al
+
+    mov al,4
+    out 0x21,al
+    mov al,2
+    out 0xa1,al
+
+    mov al,1
+    out 0x21,al
+    out 0xa1,al
+
+    mov al,11111110b
+    out 0x21,al
+    mov al,11111111b
+    out 0xa1,al
+
+    sti 
+
 
 End:
     hlt
     jmp End
 
 handler0:
+
+    push rax
+    push rbx  
+    push rcx
+    push rdx  	  
+    push rsi
+    push rdi
+    push rbp
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+    
     mov byte[0xb8000],'D'
     mov byte[0xb8001],0xc
 
     jmp End
+
+    pop	r15
+    pop	r14
+    pop	r13
+    pop	r12
+    pop	r11
+    pop	r10
+    pop	r9
+    pop	r8
+    pop	rbp
+    pop	rdi
+    pop	rsi  
+    pop	rdx
+    pop	rcx
+    pop	rbx
+    pop	rax
+
     iretq ;interrupt return
+
+Timer:
+    push rax
+    push rbx  
+    push rcx
+    push rdx  	  
+    push rsi
+    push rdi
+    push rbp
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+
+    mov byte[0xb8010],'T'
+    mov byte[0xb8011],0xe
+
+    jmp End
+
+    pop	r15
+    pop	r14
+    pop	r13
+    pop	r12
+    pop	r11
+    pop	r10
+    pop	r9
+    pop	r8
+    pop	rbp
+    pop	rdi
+    pop	rsi  
+    pop	rdx
+    pop	rcx
+    pop	rbx
+    pop	rax
+
+    iretq
+
 
 
 Gdt64:
