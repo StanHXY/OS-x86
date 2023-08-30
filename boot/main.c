50105531af4caa771382fc6f4e60e5394ab250d0
StanHXY <xhe122@syr.edu>
2023-08-30 02:06:31 +0000

    print

--- boot/main.c
+++ boot/main.c
@@ -2,9 +2,16 @@
 // fixed width data length
 #include "stddef.h"
 #include "trap.h"
+#include "print.h"
 
 void KMain(void){
 
+    char* string = "Hello";
+    int64_t value = 0x12345ABD;
+
     init_idt();
+
+    printk("%s/n", string);
+    printk("Equal to %x", value);
 
 }
