e6dcf58ecdc20d74ed3bd5421fe883ab1692eac0
StanHXY <xhe122@syr.edu>
2023-08-30 01:00:15 +0000

    trap-handler

--- boot/kernel.asm
+++ boot/kernel.asm
@@ -85,8 +85,13 @@ InitPIC:
     retf
 
 KernelEntry:
+    ;test interrupts
+    xor ax,ax
+    mov ss,ax
+
     mov rsp,0x200000
     call KMain
+    sti
 
 End:
     hlt
--- boot/main.c
+++ boot/main.c
@@ -1,12 +1,10 @@
 #include "stdint.h"
 // fixed width data length
 #include "stddef.h"
+#include "trap.h"
 
 void KMain(void){
 
-    char* p = (char*)0xb8000;
-    
-    p[0] = 'C';
-    p[1] = 0xa; // green
+    init_idt();
 
 }
